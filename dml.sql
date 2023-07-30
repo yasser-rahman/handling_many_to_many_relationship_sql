981b9e4c7dc2cf4751ef0296109cfe8e9d8804a2
yasser-rahman <65809120+yasser-rahman@users.noreply.github.com>
2023-07-30 23:55:45 +0300

    Create dml.sql

---
+++ dml.sql
@@ -0,0 +1,36 @@
+-- Primary and unique keys
+ALTER TABLE "users"
+  ADD PRIMARY KEY ("id"),
+  ADD UNIQUE ("username"),
+  ADD UNIQUE ("email");
+
+ALTER TABLE "books"
+  ADD PRIMARY KEY ("id"),
+  ADD UNIQUE ("isbn");
+
+ALTER TABLE "user_book_preferences"
+  ADD PRIMARY KEY ("user_id", "book_id");
+
+
+-- Foreign keys
+ALTER TABLE "user_book_preferences"
+  ADD FOREIGN KEY ("user_id") REFERENCES "users",
+  ADD FOREIGN KEY ("book_id") REFERENCES "books";
+
+
+-- Usernames need to have a minimum of 5 characters
+ALTER TABLE "users" ADD CHECK (LENGTH("username") >= 5);
+
+
+-- A book's name cannot be empty
+ALTER TABLE "books" ADD CHECK(LENGTH(TRIM("name")) > 0);
+
+
+-- A book's name must start with a capital letter
+ALTER TABLE "books" ADD CHECK (
+  SUBSTR("name", 1, 1) = UPPER(SUBSTR("name", 1, 1))
+);
+
+
+-- A user's book preferences have to be distinct
+ALTER TABLE "user_book_preferences" ADD UNIQUE ("user_id", "preference");
